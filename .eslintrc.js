cdab57bd497a56ad546af0de24f0d39e791d3021
James Humphreys <BinaryJim@users.noreply.github.com>
2021-12-01 10:43:58 +0000

    Squashed commit of the following:
    commit f24cacfe98165d41f7e2bbfa866f94e3a464d096
    Author: James Humphreys <BinaryJim@users.noreply.github.com>
    Date:   Wed Dec 1 10:42:14 2021 +0000
    
        Enable on demand loading of StoryBook stories
    
    commit ea0a89961473f65aba84cf278f901319aff5b59b
    Author: James Humphreys <BinaryJim@users.noreply.github.com>
    Date:   Wed Dec 1 10:39:55 2021 +0000
    
        Update storybook to version

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,30 +3,22 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
-    jest: true,
+    jest: true
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jest/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:testing-library/react",
-    "prettier",
-  ],
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "plugin:jest/recommended", "plugin:jsx-a11y/recommended", "plugin:testing-library/react", "prettier", "plugin:storybook/recommended"],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: "module"
   },
   plugins: ["react", "react-hooks", "jest", "jsx-a11y", "testing-library"],
   ignorePatterns: ["ado-client.ts", "github-client.ts"],
   rules: {},
   settings: {
     react: {
-      version: "detect",
-    },
-  },
+      version: "detect"
+    }
+  }
 };
--- .storybook/main.js
+++ .storybook/main.js
@@ -4,4 +4,8 @@ module.exports = {
     "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/react",
+  features: {
+    storyStoreV7: true,
+  },
 };
