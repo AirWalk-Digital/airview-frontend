038e75d439f6dbd7bab77cb1d6a8bc1e389954ff
James Humphreys <BinaryJim@users.noreply.github.com>
2021-12-01 11:29:59 +0000

    #132: Update AccordionMenu component to CSF 3.0

--- src/stories/accordion-menu/accordion-menu.stories.js
+++ src/stories/accordion-menu/accordion-menu.stories.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { AccordionMenu } from "../../components/accordion-menu";
 
-const config = {
+export default {
   title: "Modules/Accordion Menu",
   component: AccordionMenu,
   parameters: {
@@ -22,89 +22,88 @@ const config = {
   ],
 };
 
-const testNavItems = [
-  {
-    id: "1",
-    name: "Navigation Item 1",
-    url: "/one",
-  },
-  {
-    id: "2",
-    name: "Navigation Item Parent 1",
-    children: [
-      {
-        id: "3",
-        name: "Sub Navigation Item 1",
-        url: "/two",
-      },
-      {
-        id: "4",
-        name: "Sub Navigation Item 2",
-        url: "/three",
-      },
-      {
-        id: "5",
-        name: "Navigation Item Parent 2",
-        children: [
-          {
-            id: "6",
-            name: "Sub Navigation Item 1",
-            url: "/four",
-          },
-          {
-            id: "7",
-            name: "Sub Navigation Item 2",
-            url: "/five",
-          },
-        ],
-      },
-      {
-        id: "6",
-        name: "Sub Navigation Item 3",
-        url: "/six",
-      },
-      {
-        id: "7",
-        name: "Sub Navigation Item 4",
-        url: "/seven",
-      },
-    ],
-  },
-  {
-    id: "8",
-    name: "Navigation Item 2",
-    url: "/eight",
-  },
-  {
-    id: "9",
-    name: "Navigation Item 3",
-    url: "/nine",
-  },
-];
-
 function Template(args) {
   return <AccordionMenu {...args} />;
 }
 
 Template.args = {
-  menuTitle: "Menu Title",
-  navItems: [...testNavItems],
   id: "accordion-nav",
 };
 
-const Loading = Template.bind({});
-
-Loading.args = {
-  ...Template.args,
-  loading: true,
+export const Loading = {
+  ...Template,
+  args: {
+    ...Template.args,
+    menuTitle: "",
+    navItems: [],
+    loading: true,
+  },
 };
 
-const Loaded = Template.bind({});
-Loaded.args = {
-  ...Template.args,
-  loading: false,
+export const Loaded = {
+  ...Template,
+  args: {
+    ...Template.args,
+    menuTitle: "Menu Title",
+    navItems: [
+      {
+        id: "1",
+        name: "Navigation Item 1",
+        url: "/one",
+      },
+      {
+        id: "2",
+        name: "Navigation Item Parent 1",
+        children: [
+          {
+            id: "3",
+            name: "Sub Navigation Item 1",
+            url: "/two",
+          },
+          {
+            id: "4",
+            name: "Sub Navigation Item 2",
+            url: "/three",
+          },
+          {
+            id: "5",
+            name: "Navigation Item Parent 2",
+            children: [
+              {
+                id: "6",
+                name: "Sub Navigation Item 1",
+                url: "/four",
+              },
+              {
+                id: "7",
+                name: "Sub Navigation Item 2",
+                url: "/five",
+              },
+            ],
+          },
+          {
+            id: "6",
+            name: "Sub Navigation Item 3",
+            url: "/six",
+          },
+          {
+            id: "7",
+            name: "Sub Navigation Item 4",
+            url: "/seven",
+          },
+        ],
+      },
+      {
+        id: "8",
+        name: "Navigation Item 2",
+        url: "/eight",
+      },
+      {
+        id: "9",
+        name: "Navigation Item 3",
+        url: "/nine",
+      },
+    ],
+    loading: false,
+  },
 };
-
-export { Loading, Loaded };
-
-export default config;
