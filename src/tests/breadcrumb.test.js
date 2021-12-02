a15aa0169f744a410fecd0f87f784f4aa353dac3
James Humphreys <BinaryJim@users.noreply.github.com>
2021-12-02 12:13:56 +0000

    #132: Removed non-required Breadcrumb component tests

--- src/tests/breadcrumb.test.js
+++ src/tests/breadcrumb.test.js
@@ -2,23 +2,20 @@ import React from "react";
 import { render, screen, within } from "@testing-library/react";
 import { composeStories } from "@storybook/testing-react";
 import * as stories from "../stories/breadcrumb/breadcrumb.stories";
-import userEvent from "@testing-library/user-event";
 
 const {
-  LoadingWithoutCollapsedBreadcrumbs,
-  LoadingWithCollapsedBreadcrumbs,
+  Loading,
   LoadedWithoutCollapsedBreadcrumbs,
   LoadedWithCollapsedBreadcrumbs,
 } = composeStories(stories);
 
 describe("Breadcrumb", () => {
-  test("in a loading state, without colapsed breadcrumbs, it renders correctly", () => {
-    render(<LoadingWithoutCollapsedBreadcrumbs />);
+  test("in a loading state it renders correctly", () => {
+    render(<Loading />);
 
     const breadcrumb = screen.getByRole("navigation", { name: /breadcrumb/i });
 
     // It should have the required accesibility attributes
-    expect(breadcrumb).toHaveAttribute("aria-live", "polite");
     expect(breadcrumb).toHaveAttribute("aria-busy", "true");
 
     // It should not render any links
@@ -28,61 +25,6 @@ describe("Breadcrumb", () => {
     expect(
       within(breadcrumb).queryByRole("button", { name: /show path/i })
     ).not.toBeInTheDocument();
-
-    // It should render the correct amount of breadcrumb item placeholders
-    expect(
-      within(breadcrumb).getAllByLabelText(/breadcrumb item placeholder/i)
-    ).toHaveLength(5);
-  });
-
-  test("in a loading state, with colapsed breadcrumbs, it renders correctly", () => {
-    render(<LoadingWithCollapsedBreadcrumbs />);
-
-    const breadcrumb = screen.getByRole("navigation", { name: /breadcrumb/i });
-
-    // It should have the required accesibility attributes
-    expect(breadcrumb).toHaveAttribute("aria-live", "polite");
-    expect(breadcrumb).toHaveAttribute("aria-busy", "true");
-
-    // It should not render any links
-    expect(within(breadcrumb).queryAllByRole("link")).toHaveLength(0);
-
-    // It should render the reveal breadcrumbs button
-    expect(
-      within(breadcrumb).queryByRole("button", { name: /show path/i })
-    ).toBeInTheDocument();
-
-    // It should render the correct amount of breadcrumb item placeholders
-    expect(
-      within(breadcrumb).getAllByLabelText(/breadcrumb item placeholder/i)
-    ).toHaveLength(2);
-  });
-
-  test("in a loading state, with colapsed breadcrumbs, a user can reveal collapsed breadcrumb placeholders", () => {
-    render(<LoadingWithCollapsedBreadcrumbs />);
-
-    const breadcrumb = screen.getByRole("navigation", {
-      name: /breadcrumb/i,
-    });
-
-    const expandBreadcrumbs = within(breadcrumb).queryByRole("button", {
-      name: /show path/i,
-    });
-
-    // It should render the correct amount of breadcrumb item placeholders pre expand
-    expect(
-      within(breadcrumb).getAllByLabelText(/breadcrumb item placeholder/i)
-    ).toHaveLength(2);
-
-    userEvent.click(expandBreadcrumbs);
-
-    // On click of the expand button, the button is removed
-    expect(expandBreadcrumbs).not.toBeInTheDocument();
-
-    // It should render the correct amount of breadcrumb item placeholders post expand
-    expect(
-      within(breadcrumb).getAllByLabelText(/breadcrumb item placeholder/i)
-    ).toHaveLength(5);
   });
 
   test("in a loaded state, without collapsed breadcrumbs, it renders correctly", () => {
@@ -91,25 +33,25 @@ describe("Breadcrumb", () => {
     const breadcrumb = screen.getByRole("navigation", { name: /breadcrumb/i });
 
     // It should have the required accesibility attributes
-    expect(breadcrumb).toHaveAttribute("aria-live", "polite");
     expect(breadcrumb).toHaveAttribute("aria-busy", "false");
 
-    // It should not render any breadcrumb item placeholders
-    expect(
-      within(breadcrumb).queryAllByLabelText(/breadcrumb item placeholder/i)
-    ).toHaveLength(0);
+    const links = within(breadcrumb).getAllByRole("link");
 
-    // It should render all breadcrumb links with correct URLs
-    LoadedWithoutCollapsedBreadcrumbs.args.links.forEach((link) => {
-      const linkItem = within(breadcrumb).getByRole("link", {
-        name: link.label,
-      });
+    // It should render all breadcrumb links
+    expect(links).toHaveLength(4);
 
-      expect(linkItem).toBeInTheDocument();
-      expect(linkItem).toHaveAttribute("href", linkItem.url);
-    });
+    // It should apply the correct label to the link
+    expect(links[0]).toHaveTextContent(
+      LoadedWithoutCollapsedBreadcrumbs.args.links[0].label
+    );
 
-    // It renders the current page as static text
+    // It should apply the correct URL to the link
+    expect(links[0]).toHaveAttribute(
+      "href",
+      LoadedWithoutCollapsedBreadcrumbs.args.links[0].url
+    );
+
+    // It should output the current page
     expect(
       within(breadcrumb).getByText(
         LoadedWithoutCollapsedBreadcrumbs.args.activeRoute
@@ -122,58 +64,15 @@ describe("Breadcrumb", () => {
 
     const breadcrumb = screen.getByRole("navigation", { name: /breadcrumb/i });
 
-    // It should have the required accesibility attributes
-    expect(breadcrumb).toHaveAttribute("aria-live", "polite");
-    expect(breadcrumb).toHaveAttribute("aria-busy", "false");
-
-    // It should not render any breadcrumb item placeholders
-    expect(
-      within(breadcrumb).queryAllByLabelText(/breadcrumb item placeholder/i)
-    ).toHaveLength(0);
+    const breadcrumbLinks = within(breadcrumb).getAllByRole("link");
 
     // It should render a partial subset of links
-    const breadcrumbLinks = within(breadcrumb).queryAllByRole("link");
-
     expect(breadcrumbLinks).toHaveLength(1);
-    expect(breadcrumbLinks[0]).toHaveTextContent(
-      LoadedWithCollapsedBreadcrumbs.args.links[0].label
-    );
-
-    // It renders the current page as static text
-    expect(
-      within(breadcrumb).getByText(
-        LoadedWithCollapsedBreadcrumbs.args.activeRoute
-      )
-    ).toBeInTheDocument();
-  });
-
-  test("in a loaded state, with collapsed breadcrumbs, a user can reveal collapsed breadcrumbs", () => {
-    render(<LoadedWithCollapsedBreadcrumbs />);
-
-    const breadcrumb = screen.getByRole("navigation", { name: /breadcrumb/i });
-
-    const expandBreadcrumbsBtn = within(breadcrumb).queryByRole("button", {
-      name: /show path/i,
-    });
-
-    // It should render the correct amount of breadcrumb links pre expand
-    expect(within(breadcrumb).getAllByRole("link")).toHaveLength(1);
-
-    // On click of expand breadbrumbs button
-    userEvent.click(expandBreadcrumbsBtn);
-
-    // It should reveal all breadcrumb links
-    expect(within(breadcrumb).getAllByRole("link")).toHaveLength(
-      LoadedWithCollapsedBreadcrumbs.args.links.length
-    );
-
-    // It should remove the expand breadcrumbs button
-    expect(expandBreadcrumbsBtn).not.toBeInTheDocument();
   });
 
   test("it should spread other props to the component root DOM node", () => {
     const testId = "breadcrumb-test";
-    render(<LoadingWithoutCollapsedBreadcrumbs data-testid={testId} />);
+    render(<LoadedWithoutCollapsedBreadcrumbs data-testid={testId} />);
 
     const breadcrumb = screen.getByRole("navigation", { name: /breadcrumb/i });
 
