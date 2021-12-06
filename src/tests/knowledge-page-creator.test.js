9cfabcd79e18e13f657075d6d6cf793fb674895f
James Humphreys <BinaryJim@users.noreply.github.com>
2021-12-06 15:37:32 +0000

    #132: Add tests for KnowledgePageCreator

--- src/tests/knowledge-page-creator.test.js
+++ src/tests/knowledge-page-creator.test.js
@@ -1,5 +1,44 @@
-describe("KnowledgePageCreator", () => {
-  test.todo("a user can create a new Knowledge page");
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import { composeStories } from "@storybook/testing-react";
+import * as stories from "../stories/knowledge-page-creator/knowledge-page-creator.stories";
 
-  test.todo("a user is alerted to an error, if thrown");
+const { WithSubmissionSuccess, WithSubmissionError } = composeStories(stories);
+
+describe("KnowledgePageCreator", () => {
+  test("a user can create a new Knowledge page", async () => {
+    const onSubmitSpy = jest.spyOn(WithSubmissionSuccess.args, "onSubmit");
+
+    const { container } = render(<WithSubmissionSuccess />);
+
+    await WithSubmissionSuccess.play({ canvasElement: container });
+
+    await waitFor(() => {
+      expect(onSubmitSpy).toHaveBeenCalledTimes(1);
+      expect(onSubmitSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          title: expect.any(String),
+          reviewDate: expect.any(String),
+          userFacing: expect.any(Boolean),
+        })
+      );
+      expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+    });
+
+    onSubmitSpy.mockRestore();
+  });
+
+  test("a user is alerted to an error, if thrown", async () => {
+    const onSubmitSpy = jest.spyOn(WithSubmissionError.args, "onSubmit");
+
+    const { container } = render(<WithSubmissionError />);
+
+    await WithSubmissionError.play({ canvasElement: container });
+
+    await waitFor(() => {
+      expect(screen.getByRole("alert")).toBeInTheDocument();
+    });
+
+    onSubmitSpy.mockRestore();
+  });
 });
