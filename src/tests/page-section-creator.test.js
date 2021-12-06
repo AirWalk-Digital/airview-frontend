10dc017df853e8b519d90d345f6087f20c1ae0a6
James Humphreys <BinaryJim@users.noreply.github.com>
2021-12-06 16:49:38 +0000

    #132: Added tests for PageSectionCreator

--- src/tests/page-section-creator.test.js
+++ src/tests/page-section-creator.test.js
@@ -1,5 +1,38 @@
-describe("PageSectionCreator", () => {
-  test.todo("a user can create a new page section");
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import { composeStories } from "@storybook/testing-react";
+import * as stories from "../stories/page-section-creator/page-section-creator.stories";
 
-  test.todo("a user is alerted to an error, if thrown");
+const { WithSubmissionSuccess, WithSubmissionError } = composeStories(stories);
+
+describe("PageSectionCreator", () => {
+  test("a user can create a new page section", async () => {
+    const onSubmitSpy = jest.spyOn(WithSubmissionSuccess.args, "onSubmit");
+
+    const { container } = render(<WithSubmissionSuccess />);
+
+    await WithSubmissionSuccess.play({ canvasElement: container });
+
+    await waitFor(() => {
+      expect(onSubmitSpy).toHaveBeenCalledTimes(1);
+      expect(onSubmitSpy).toHaveBeenCalledWith(expect.any(String));
+      expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+    });
+
+    onSubmitSpy.mockRestore();
+  });
+
+  test("a user is alerted to an error, if thrown", async () => {
+    const onSubmitSpy = jest.spyOn(WithSubmissionError.args, "onSubmit");
+
+    const { container } = render(<WithSubmissionError />);
+
+    await WithSubmissionError.play({ canvasElement: container });
+
+    await waitFor(() => {
+      expect(screen.getByRole("alert")).toBeInTheDocument();
+    });
+
+    onSubmitSpy.mockRestore();
+  });
 });
