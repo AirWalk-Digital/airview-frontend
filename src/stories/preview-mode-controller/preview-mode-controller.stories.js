ba2a86d7a6be648f006093f02e370d4e135cc669
James Humphreys <BinaryJim@users.noreply.github.com>
2021-12-07 16:03:20 +0000

    #132: Re-write of PreviewModeController stories

--- src/stories/preview-mode-controller/preview-mode-controller.stories.js
+++ src/stories/preview-mode-controller/preview-mode-controller.stories.js
@@ -1,5 +1,13 @@
 import React from "react";
-import dayjs from "dayjs";
+import * as BranchSwitcherStories from "../branch-switcher/branch-switcher.stories";
+import * as BranchCreatorStories from "../branch-creator/branch-creator.stories";
+import * as KnowledgePageCreatorStories from "../knowledge-page-creator/knowledge-page-creator.stories";
+import * as KnowledgePageMetaEditorStories from "../knowledge-page-meta-editor/knowledge-page-meta-editor.stories";
+import * as PageSectionCreatorStories from "../page-section-creator/page-section-creator.stories";
+import * as ApplicationCreatorStories from "../application-creator/application-creator.stories";
+import * as ContentCommitterStories from "../content-committer/content-committer.stories";
+import * as PullRequestCreatorStories from "../pull-request-creator/pull-request-creator.stories";
+
 import {
   PreviewModeController,
   BranchCreator,
@@ -12,7 +20,7 @@ import {
   PullRequestCreator,
 } from "../../components/preview-mode-controller";
 
-const config = {
+export default {
   title: "Modules/Preview Mode Controller",
   component: PreviewModeController,
   subcomponents: {
@@ -28,291 +36,49 @@ const config = {
   parameters: {
     layout: "padded",
   },
+  argTypes: {
+    children: {
+      control: false,
+    },
+  },
 };
 
-function makeSubComponentArgs(rejectPromise) {
-  const delay = process.env.NODE_ENV === "test" ? 0 : 1000;
-  const log = process.env.NODE_ENV !== "test";
-
-  return {
-    branchSwitcherArgs: {
-      onSubmit: (branchName) => {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            log && console.log(branchName);
-
-            if (rejectPromise) {
-              reject({
-                error: "Error: Unable to switch branch, please try again",
-              });
-            } else {
-              resolve();
-            }
-          }, delay);
-        });
-      },
-    },
-    branchCreatorArgs: {
-      onSubmit: (branchName) => {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            log && console.log(branchName);
-
-            if (rejectPromise) {
-              reject({
-                error: "Error: Unable to create branch, please try again",
-              });
-            } else {
-              resolve();
-            }
-          }, delay);
-        });
-      },
-    },
-    knowledgePageCreatorArgs: {
-      onSubmit: (formData) => {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            log && console.log(formData);
-
-            if (rejectPromise) {
-              reject({
-                error:
-                  "Error: Unable to create a new Knowledge page, please try again",
-              });
-            } else {
-              resolve();
-            }
-          }, delay);
-        });
-      },
-    },
-    knowledgePageMetaEditorArgs: {
-      onSubmit: (formData) => {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            log && console.log(formData);
-
-            if (rejectPromise) {
-              reject({
-                error:
-                  "Error: Unable to submit page meta changes, please try again",
-              });
-            } else {
-              resolve();
-            }
-          }, delay);
-        });
-      },
-      initialData: {
-        title: "Test document title",
-        reviewDate: dayjs().subtract(1, "day").toISOString(),
-        userFacing: true,
-      },
-    },
-    pageSectionCreatorArgs: {
-      onSubmit: (sectionName) => {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            log && console.log(sectionName);
-
-            if (rejectPromise) {
-              reject({
-                error:
-                  "Error: Unable to create a page section, please try again",
-              });
-            } else {
-              resolve();
-            }
-          }, delay);
-        });
-      },
-    },
-    applicationCreatorArgs: {
-      applications: [
-        {
-          name: "Application One",
-          id: 1,
-        },
-        {
-          name: "Application Two",
-          id: 2,
-        },
-      ],
-      applicationTypes: [
-        {
-          name: "Type One",
-          id: 1,
-        },
-        {
-          name: "Type Two",
-          id: 2,
-        },
-      ],
-      environments: [
-        {
-          name: "Environment One",
-          id: 1,
-        },
-        {
-          name: "Environment Two",
-          id: 2,
-        },
-      ],
-      referenceTypes: ["type_one", "type_two"],
-      onSubmit: (formData) => {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            log && console.log(formData);
-
-            if (rejectPromise) {
-              reject({
-                error: "Error: Unable to create application, please try again",
-              });
-            } else {
-              resolve();
-            }
-          }, delay);
-        });
-      },
-    },
-    contentCommitterArgs: {
-      onSubmit: () => {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            log && console.log("Committing changes");
-
-            if (rejectPromise) {
-              reject({
-                error: "Error: Unable to commit changes, please try again",
-              });
-            } else {
-              resolve();
-            }
-          }, delay);
-        });
-      },
-    },
-    pullRequestCreatorArgs: {
-      onSubmit: (from, to) => {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            log &&
-              console.log(`PR request from branch "${from}" to branch "${to}"`);
-
-            if (rejectPromise) {
-              reject({
-                error:
-                  "Error: Unable to create a pull request, please try again",
-              });
-            } else {
-              resolve("https://github.com");
-            }
-          }, delay);
-        });
-      },
-    },
-  };
-}
-
 function Template(args) {
-  const {
-    branchSwitcherArgs,
-    branchCreatorArgs,
-    knowledgePageCreatorArgs,
-    knowledgePageMetaEditorArgs,
-    pageSectionCreatorArgs,
-    applicationCreatorArgs,
-    contentCommitterArgs,
-    pullRequestCreatorArgs,
-    ...rest
-  } = args;
-
   return (
-    <PreviewModeController {...rest}>
-      <BranchSwitcher {...branchSwitcherArgs} />
-      <BranchCreator {...branchCreatorArgs} />
-      <KnowledgePageCreator {...knowledgePageCreatorArgs} />
-      <KnowledgePageMetaEditor {...knowledgePageMetaEditorArgs} />
-      <PageSectionCreator {...pageSectionCreatorArgs} />
-      <ApplicationCreator {...applicationCreatorArgs} />
-      <ContentCommitter {...contentCommitterArgs} />
-      <PullRequestCreator {...pullRequestCreatorArgs} />
+    <PreviewModeController {...args}>
+      <BranchSwitcher {...BranchSwitcherStories.Closed.args} />
+      <BranchCreator {...BranchCreatorStories.Closed.args} />
+      <KnowledgePageCreator {...KnowledgePageCreatorStories.Closed.args} />
+      <KnowledgePageMetaEditor
+        {...KnowledgePageMetaEditorStories.ClosedNotDisabled.args}
+      />
+      <PageSectionCreator {...PageSectionCreatorStories.Closed.args} />
+      <ApplicationCreator {...ApplicationCreatorStories.Closed.args} />
+      <ContentCommitter {...ContentCommitterStories.Closed.args} />
+      <PullRequestCreator {...PullRequestCreatorStories.Closed.args} />
     </PreviewModeController>
   );
 }
 
 Template.args = {
-  enabled: false,
-  onToggle: () => {},
   branches: [
-    { name: "master", protected: true },
+    { name: "main", protected: true },
     { name: "development", protected: false },
   ],
   workingRepo: "test-org/test-repository",
   workingBranch: "development",
-  baseBranch: "master",
-  loading: true,
-  ...makeSubComponentArgs(),
+  baseBranch: "main",
 };
 
-Template.argTypes = {
-  branchSwitcherArgs: {
-    table: {
-      disable: true,
-    },
-  },
-  branchCreatorArgs: {
-    table: {
-      disable: true,
-    },
-  },
-  knowledgePageCreatorArgs: {
-    table: {
-      disable: true,
-    },
-  },
-  knowledgePageMetaEditorArgs: {
-    table: {
-      disable: true,
-    },
-  },
-  pageSectionCreatorArgs: {
-    table: {
-      disable: true,
-    },
-  },
-  applicationCreatorArgs: {
-    table: {
-      disable: true,
-    },
-  },
-  contentCommitterArgs: {
-    table: {
-      disable: true,
-    },
-  },
-  pullRequestCreatorArgs: {
-    table: {
-      disable: true,
-    },
-  },
-  children: {
-    control: false,
-  },
-};
-
-const Loading = Template.bind({});
+export const Loading = Template.bind({});
 
 Loading.args = {
   ...Template.args,
+  enabled: false,
+  loading: true,
 };
 
-Loading.argTypes = {
-  ...Template.argTypes,
-};
-
-const Inactive = Template.bind({});
+export const Inactive = Template.bind({});
 
 Inactive.args = {
   ...Template.args,
@@ -320,34 +86,10 @@ Inactive.args = {
   loading: false,
 };
 
-Inactive.argTypes = {
-  ...Template.argTypes,
-};
-
-const Active = Template.bind({});
+export const Active = Template.bind({});
 
 Active.args = {
   ...Template.args,
   enabled: true,
   loading: false,
 };
-
-Active.argTypes = {
-  ...Template.argTypes,
-};
-
-const ActiveWithErrors = Template.bind({});
-
-ActiveWithErrors.args = {
-  ...Template.args,
-  enabled: true,
-  loading: false,
-  ...makeSubComponentArgs(true),
-};
-
-ActiveWithErrors.argTypes = {
-  ...Template.argTypes,
-};
-
-export default config;
-export { Loading, Inactive, Active, ActiveWithErrors };
