7347ae193842c8baab536036d0f1e144dd7c0208
James Humphreys <BinaryJim@users.noreply.github.com>
2021-12-08 14:39:09 +0000

    #132: Fix linting issues and update to CSF 3.0 for ProgressBar stories

--- src/stories/progress-bar/progress-bar.stories.js
+++ src/stories/progress-bar/progress-bar.stories.js
@@ -2,9 +2,30 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { ProgressBar } from "../../components/progress-bar";
 
-const config = {
+export default {
   title: "Modules/ProgressBar",
   component: ProgressBar,
+  argTypes: {
+    ariaLabel: {
+      control: false,
+    },
+    classNames: {
+      control: false,
+    },
+    color: {
+      control: {
+        type: "color",
+      },
+    },
+    value: {
+      control: {
+        type: "number",
+        min: 0,
+        max: 100,
+        step: 1,
+      },
+    },
+  },
   decorators: [
     (story) => {
       const classes = makeStyles(() => ({
@@ -30,73 +51,37 @@ function Template(args) {
   return <ProgressBar {...args} />;
 }
 
-Template.argTypes = {
-  ariaLabel: {
-    control: false,
-  },
-  classNames: {
-    control: false,
-  },
-  color: {
-    control: {
-      type: "color",
-    },
-  },
-  value: {
-    control: {
-      type: "number",
-      min: 0,
-      max: 100,
-      step: 1,
-    },
+export const Default = {
+  ...Template,
+  args: {
+    ...Template.args,
+    value: 33,
   },
 };
 
-const Default = Template.bind({});
-
-Default.args = {
-  value: 33,
-};
-
-Default.argTypes = {
-  ...Template.argTypes,
-};
-
-const WithoutLabel = Template.bind({});
-
-WithoutLabel.args = {
-  value: 33,
-  showLabel: false,
-};
-
-WithoutLabel.argTypes = {
-  ...Template.argTypes,
-};
-
-const Determinate = Template.bind({});
-
-Determinate.args = {
-  value: 33,
-  variant: "determinate",
-};
-
-Determinate.argTypes = {
-  ...Template.argTypes,
-};
-
-const Indeterminate = Template.bind({});
-
-Indeterminate.args = {
-  variant: "indeterminate",
-  showLabel: false,
-};
-
-Indeterminate.argTypes = {
-  ...Template.argTypes,
-  value: {
-    control: false,
+export const WithoutLabel = {
+  ...Template,
+  args: {
+    ...Template.args,
+    value: 33,
+    showLabel: false,
   },
 };
 
-export { Default, WithoutLabel, Determinate, Indeterminate };
-export default config;
+export const Determinate = {
+  ...Template,
+  args: {
+    ...Template.args,
+    value: 33,
+    variant: "determinate",
+  },
+};
+
+export const Indeterminate = {
+  ...Template,
+  args: {
+    ...Template.args,
+    variant: "indeterminate",
+    showLabel: false,
+  },
+};
