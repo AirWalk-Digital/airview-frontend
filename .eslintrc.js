530ca7a0a392068c8ba95c14a613951529a36be4
James Humphreys <BinaryJim@users.noreply.github.com>
2021-12-10 10:30:35 +0000

    #144: Format eslint config and relocate prettier extension as last item

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,31 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
-    jest: true
+    jest: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "plugin:jest/recommended", "plugin:jsx-a11y/recommended", "plugin:testing-library/react", "prettier", "plugin:storybook/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jest/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:testing-library/react",
+    "plugin:storybook/recommended",
+    "prettier",
+  ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["react", "react-hooks", "jest", "jsx-a11y", "testing-library"],
   ignorePatterns: ["ado-client.ts", "github-client.ts"],
   rules: {},
   settings: {
     react: {
-      version: "detect"
-    }
-  }
+      version: "detect",
+    },
+  },
 };
