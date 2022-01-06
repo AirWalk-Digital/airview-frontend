b975ef76c32609c4aa4ee7f71f44b94f11f5a14b
James Humphreys <BinaryJim@users.noreply.github.com>
2022-01-06 16:20:11 +0000

    #151: Added LayoutBase component / layout

---
+++ src/components/layout-base/layout-base.js
@@ -0,0 +1,116 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container } from "@material-ui/core";
+import { Helmet } from "react-helmet";
+import { PageHeader } from "../page-header";
+import { Breadcrumb } from "../breadcrumb";
+import { LocationProvider } from "../../hooks/use-location";
+
+export function LayoutBase({
+  currentRoute,
+  pageTitle,
+  siteTitle,
+  version,
+  logoSrc,
+  navItems,
+  loading,
+  onQueryChange,
+  previewMode,
+  breadcrumbLinks,
+  children,
+}) {
+  const styles = useStyles();
+
+  return (
+    <LocationProvider location={currentRoute}>
+      <Helmet>
+        <title>{`${pageTitle} | ${siteTitle}`}</title>
+      </Helmet>
+
+      <PageHeader
+        {...{
+          siteTitle,
+          version,
+          logoSrc,
+          navItems,
+          loading,
+          onQueryChange,
+          previewMode,
+        }}
+      />
+
+      <Container>
+        <Breadcrumb
+          links={breadcrumbLinks}
+          loading={loading}
+          activeRoute={pageTitle}
+          className={styles.breadcrumb}
+        />
+      </Container>
+
+      {children && (
+        <Container className={styles.mainContainer}>{children}</Container>
+      )}
+    </LocationProvider>
+  );
+}
+
+const useStyles = makeStyles((theme) => {
+  return {
+    breadcrumb: {
+      paddingTop: theme.spacing(6),
+    },
+    mainContainer: {
+      paddingTop: theme.spacing(4),
+      paddingBottom: theme.spacing(8),
+    },
+  };
+});
+
+LayoutBase.propTypes = {
+  /**
+   * Used to set the required context for link active classes
+   */
+  currentRoute: PropTypes.string.isRequired,
+  /**
+   * Sets the meta title in the document head
+   */
+  pageTitle: PropTypes.string.isRequired,
+  /**
+   * Used to set the meta title in the document head and populate sub component data
+   */
+  siteTitle: PropTypes.string.isRequired,
+  /**
+   * Used to set the version number in the main header menu
+   */
+  version: PropTypes.string.isRequired,
+  /**
+   * Used to render the logo in the main header
+   */
+  logoSrc: PropTypes.string.isRequired,
+  /**
+   * Used to render the main navigation in the main header (see [AccordionMenu](/?path=/docs/modules-accordion-menu) `navItems` prop API for schema)
+   */
+  navItems: PropTypes.array.isRequired,
+  /**
+   * Presents the component in a loading state (for when fetching data async)
+   */
+  loading: PropTypes.bool.isRequired,
+  /**
+   * Callback fired when the user has changed the query input value of the search UI. see [Search](/?path=/docs/modules-search--single-result-found) `onQueryChange` API for details
+   */
+  onQueryChange: PropTypes.func.isRequired,
+  /**
+   * Used to render or hide the content edit button
+   */
+  previewMode: PropTypes.bool.isRequired,
+  /**
+   * Used to set the breadcrumb navigation (see [Breadcumb](/?path=/docs/modules-breadcrumb) `links` prop API for schema
+   */
+  breadcrumbLinks: PropTypes.array.isRequired,
+  /**
+   * Content to be pased to the main page section
+   */
+  children: PropTypes.node,
+};
---
+++ src/stories/layout-base/layout-base.stories.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Title, ArgsTable } from "@storybook/addon-docs";
+import { makeStyles } from "@material-ui/core/styles";
+import * as PageHeaderStories from "../page-header/page-header.stories";
+import * as BreadcrumbStories from "../breadcrumb/breadcrumb.stories";
+import { LayoutBase } from "../../components/layout-base";
+
+export default {
+  title: "Layouts/Layout Base",
+  component: LayoutBase,
+  parameters: {
+    layout: "fullscreen",
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <ArgsTable />
+        </>
+      ),
+    },
+  },
+  decorators: [
+    (story) => {
+      const classes = makeStyles(() => ({
+        "@global": {
+          "#docs-root .MuiAppBar-root": {
+            position: "initial",
+          },
+        },
+        root: {},
+      }))();
+
+      return <div className={classes.root}>{story()}</div>;
+    },
+  ],
+};
+
+function Template(args) {
+  return <LayoutBase {...args} />;
+}
+
+Template.args = {
+  currentRoute: "/",
+  pageTitle: "Layout Base",
+};
+
+export const Loading = {
+  ...Template,
+  args: {
+    ...Template.args,
+    ...PageHeaderStories.Loading.args,
+    breadcrumbLinks: [],
+  },
+};
+
+export const LoadedPreviewDisabled = {
+  ...Template.args,
+  args: {
+    ...Template.args,
+    ...PageHeaderStories.PreviewDisabled.args,
+    breadcrumbLinks:
+      BreadcrumbStories.LoadedWithoutCollapsedBreadcrumbs.args.links,
+  },
+};
+
+export const LoadedPreviewEnabled = {
+  ...Template.args,
+  args: {
+    ...Template.args,
+    ...PageHeaderStories.PreviewEnabled.args,
+    breadcrumbLinks:
+      BreadcrumbStories.LoadedWithoutCollapsedBreadcrumbs.args.links,
+  },
+};
